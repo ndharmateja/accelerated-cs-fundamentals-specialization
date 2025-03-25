4aa35f6996590a6eab5332f26cceea1bfce5a3a5
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2025-03-25 11:42:30 +0530

    change printing format of cube

--- 1-object-oriented-data-structures-in-c++/module1/classes/main.cpp
+++ 1-object-oriented-data-structures-in-c++/module1/classes/main.cpp
@@ -4,13 +4,14 @@
 int main(int argc, char const *argv[])
 {
     Cube c;
-    c.setLength(5.5);
-    std::cout << "Cube{length: "
-              << c.getLength()
-              << ", surface area: "
-              << c.getSurfaceArea()
-              << ", volume: "
-              << c.getVolume()
+    c.setLength(3.48);
+    std::cout << "Cube {" << std::endl
+              << "    length: "
+              << c.getLength() << std::endl
+              << "    surface area: "
+              << c.getSurfaceArea() << std::endl
+              << "    volume: "
+              << c.getVolume() << std::endl
               << "}" << std::endl;
     return 0;
 }
