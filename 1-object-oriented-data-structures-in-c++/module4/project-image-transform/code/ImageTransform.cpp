fcda13dab1d09b9bd1f8f3a8c59ec2ec66418057
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2025-03-27 12:37:01 +0530

    add watermark function

--- 1-object-oriented-data-structures-in-c++/module4/project-image-transform/code/ImageTransform.cpp
+++ 1-object-oriented-data-structures-in-c++/module4/project-image-transform/code/ImageTransform.cpp
@@ -118,18 +118,30 @@ PNG illinify(PNG image)
 }
 
 /**
-* Returns an immge that has been watermarked by another image.
-*
-* The luminance of every pixel of the second image is checked, if that
-* pixel's luminance is 1 (100%), then the pixel at the same location on
-* the first image has its luminance increased by 0.2.
-*
-* @param firstImage  The first of the two PNGs, which is the base image.
-* @param secondImage The second of the two PNGs, which acts as the stencil.
-*
-* @return The watermarked image.
-*/
-PNG watermark(PNG firstImage, PNG secondImage) {
+ * Returns an immge that has been watermarked by another image.
+ *
+ * The luminance of every pixel of the second image is checked, if that
+ * pixel's luminance is 1 (100%), then the pixel at the same location on
+ * the first image has its luminance increased by 0.2.
+ *
+ * @param base  The first of the two PNGs, which is the base image.
+ * @param stencil The second of the two PNGs, which acts as the stencil.
+ *
+ * @return The watermarked image.
+ */
+PNG watermark(PNG base, PNG stencil)
+{
+    for (unsigned x = 0; x < min(base.width(), stencil.width()); x++)
+    {
+        for (unsigned y = 0; y < min(base.height(), stencil.height()); y++)
+        {
+            HSLAPixel &stencilPixel = stencil.getPixel(x, y);
+            if (stencilPixel.l != 1.0)
+                continue;
 
-  return firstImage;
+            HSLAPixel &basePixel = base.getPixel(x, y);
+            basePixel.l = min(basePixel.l + 0.2, 1.0);
+        }
+    }
+    return base;
 }
