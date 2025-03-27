e085f504a45d948401d07f6c50f82ec6ee03138b
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2025-03-27 23:04:04 +0530

    add arrays code

--- 2-ordered-data-structures/1-arrays/main.cpp
+++ 2-ordered-data-structures/1-arrays/main.cpp
@@ -5,7 +5,11 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     int values[5] = {2, 3, 5, 7, 11};
+    cout << "values[3]: " << values[3] << endl;
 
-    cout << values[3] << endl;
+    cout << "size of int: " << sizeof(int) << endl;
+    int offset = (long)(values + 2) - (long)values;
+    cout << "offset (bytes) between 0 and 2: " << offset << endl;
+
     return 0;
 }
